d218d23859c16bc9a5e01670af928ac67d229c75
dhivyaapv <143778604+dhivyaapv@users.noreply.github.com>
2026-08-24 20:50:49 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "/api",
 });
 
-// Interceptor to dynamically attach token to every request
+// Attach JWT token to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => {
